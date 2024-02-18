0e3e53f93f839fa4364d4a4ce75986959cdd8a33
rocean <rocean74@gmail.com>
2024-02-18 22:06:35 +0200

    Improve code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,37 +2,60 @@ import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) {
-        BigInteger n = new BigInteger("18784930108456612406607814982869320937100357623565293065520882099446807661280672536412900261980252211590001129415416692842708423735233445954982126337452630953642389398728359447338862517090153799904393322766932296070412642108354965913688213429893600565940214997335063029350129915211361225154608407197811535838113388016141398750885113342334046258520069354875722557429638792502313832152491728469179017286106563712702581115089781944984882457716639211444194974007013843331628459682686708234290546709545912155519705333002154730995212015948695451676666505480477641054802308304079577983185418820145281416757335093826951423117");
+        // Small number example
+//        BigInteger n = new BigInteger("10142789312725007");
 
-        // Calculate a = sqrt(n) + 1
-        BigInteger a = n.sqrt().add(BigInteger.ONE);
+        // Video example
+//        BigInteger n = new BigInteger("5261933844650100908430030083398098838688018147149529533465444719385566864605781576487305356717074882505882701585297765789323726258356035692769897420620858774763694117634408028918270394852404169072671551096321238430993811080749636806153881798472848720411673994908247486124703888115308603904735959457057925225503197625820670522050494196703154086316062123787934777520599894745147260327060174336101658295022275013051816321617046927321006322752178354002696596328204277122466231388232487691224076847557856202947748540263791767128195927179588238799470987669558119422552470505956858217654904628177286026365989987106877656917");
 
-        System.out.println("a = " + a);
+        BigInteger n = new BigInteger("84641628266040968134726266858994621642842114704041843715900121029779624800354442181024933538554572539172519633298583132480574427234887384864237862510536135711979678422472297405718744900411759436052738392404109005261750528594360602400820678382015390265136033037813004859176942808586218232493968301780230653856757977924165795192647059713220241170741461160395394356567033643278968175549740103490059728058303074181319743641739740188742499285217435763085176127055686256451491791548801388560202149363758975803673725819798718410301085155062206092370667133063831263112034304132340281237240943617970645337927088718346956199378919");
 
-        // Calculate b = a^2 - n until b is a perfect square
+        int bits = n.bitLength();
+        System.out.println("Number of bits: " + bits);
 
-        BigInteger b;
-
-        while (true) {
-            BigInteger b2 = a.pow(2).subtract(n);
-            // if b2 is a perfect square
-            if (isPerfectSquare(b2)) {
-                System.out.println("b2 = " + b2);
-                b = b2.sqrt();
-                break;
-            } else {
-                a = a.add(BigInteger.ONE);
-                System.out.println("a = " + a);
+        if (n.mod(BigInteger.valueOf(2)).equals(BigInteger.ZERO)) {
+            System.out.println("2 * " + n.divide(BigInteger.valueOf(2)));
+        } else {
+            try {
+                System.out.println("Fermat factorization of " + n + " is: ");
+                BigInteger[] factors = fermatFactorization(n);
+                System.out.println(factors[0] + " * " + factors[1]);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         }
-
-        System.out.println("n (maybe) = " + a.multiply(b));
-        System.out.println("n = " + n);
     }
 
     public static boolean isPerfectSquare(BigInteger n) {
         BigInteger sqrt = n.sqrt();
+
         return sqrt.pow(2).equals(n);
+    }
+
+    // Method to calculate fermat factorization
+    public static BigInteger[] fermatFactorization(BigInteger number) {
+        BigInteger a = number.sqrt().add(BigInteger.ONE);
+        System.out.println("a: " + a);
+        BigInteger b = null;
+        int tries = 100;
+
+        while (tries-- > 0) {
+            BigInteger b2 = a.pow(2).subtract(number);
+
+            if (isPerfectSquare(b2)) {
+                b = b2.sqrt();
+                break;
+            }
+
+            a = a.add(BigInteger.ONE);
+        }
+
+        if (b == null) {
+            throw new RuntimeException("Fermat factorization failed");
+        }
+
+        System.out.println("b: " + b);
+        return new BigInteger[]{a.subtract(b), a.add(b)};
     }
 }
 
